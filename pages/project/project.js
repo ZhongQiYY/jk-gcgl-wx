5bf55dd718c48375217b1a1fe6dac4130df73c01
ZhongQiYY <2378544112@qq.com>
2020-07-06 23:23:30 +0800

    commit

--- pages/project/project.js
+++ pages/project/project.js
@@ -9,21 +9,21 @@ Page({
     categoryIndex: 0,
 
     projectList: [
-      {id: "", projectName: "华昌新材料产业园（能之光代建）标准厂房", explain: "新建项目"},
-      {id: "", projectName: "华昌科技园标准厂房", explain: "新建项目"},
-      {id: "", projectName: "秋山科技园标准厂房", explain: "新建项目"},
-      {id: "", projectName: "江奥科技园标准厂房", explain: "新建项目"},
-      {id: "", projectName: "涌泉科技园标准厂房", explain: "续建项目"},
-      {id: "", projectName: "横江产业园标准厂房", explain: "续建项目"},
-      {id: "", projectName: "金凤智谷一期项目", explain: "续建项目"},
-      {id: "", projectName: "秋月科技园标准厂房", explain: "新建项目"},
-      {id: "", projectName: "春花科技园", explain: "续建项目"},
-      {id: "", projectName: "横江产业园标准厂房", explain: "新建项目"},
+      {id: "1", projectName: "华昌新材料产业园（能之光代建）标准厂房", explain: "新建项目"},
+      {id: "2", projectName: "华昌科技园标准厂房", explain: "新建项目"},
+      {id: "3", projectName: "秋山科技园标准厂房", explain: "新建项目"},
+      {id: "4", projectName: "江奥科技园标准厂房", explain: "新建项目"},
+      {id: "5", projectName: "涌泉科技园标准厂房", explain: "续建项目"},
+      {id: "6", projectName: "横江产业园标准厂房", explain: "续建项目"},
+      {id: "7", projectName: "金凤智谷一期项目", explain: "续建项目"},
+      {id: "8", projectName: "秋月科技园标准厂房", explain: "新建项目"},
+      {id: "9", projectName: "春花科技园", explain: "续建项目"},
+      {id: "10", projectName: "横江产业园标准厂房", explain: "新建项目"},
     ]
   },
 
   // 选择建设单位
-  bindBuildChange: function(e) {
+  bindBuildChange: function(e) {  
     this.setData({
       buildIndex: e.detail.value
     })
