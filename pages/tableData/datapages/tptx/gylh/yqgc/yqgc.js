2ff097bb1c1ad01de62927f75ba588b8bdb5ac6a
18779918803 <814883929@qq.com>
2020-08-26 09:42:34 +0800

    a

--- pages/tableData/datapages/tptx/gylh/yqgc/yqgc.js
+++ pages/tableData/datapages/tptx/gylh/yqgc/yqgc.js
@@ -1,11 +1,13 @@
-// pages/tableData/datapages/tptx/gylh/yqgc/yqgc.js
+const app = getApp();
+const RootPath = "http://localhost:16000";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    pmGylhl:{},
+    RootPath: RootPath
   },
 
   /**
@@ -22,11 +24,48 @@ Page({
 
   },
 
+  //查询接口，onshow中调用
+  getYqgc: function(){
+    console.log("调用getYqgc");
+    var that = this;
+    wx.request({
+      url: RootPath + "/jk-gcgl/api/tptx/pmTptx/yqgc?pId="+14, //请求路径
+      method: 'post',
+      data: {
+        
+      },
+      header: {
+        'content-type': 'application/json', // 默认值
+        'thirdSession': app.globalData.thirdSession
+      },
+      success (res) {
+        if (res.statusCode == 200) {
+          console.log(res.data);
+          var qq = res.data.data
+          console.log(qq)
+          if (res.data.code == 200) {
+            that.setData({
+              pmGylhl: res.data.data,
+            })
+          }else{
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'success',
+              duration: 2000
+            });
+            console.log("finish.js getYqgc() error res:----->>>>");
+            console.log(res);
+          }
+        }
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this;
+    that.getYqgc();
   },
 
   /**
