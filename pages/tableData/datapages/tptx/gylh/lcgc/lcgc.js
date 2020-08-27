cb6e3ce47438a56c6520e9b9ff602ce5cf172a19
18779918803 <814883929@qq.com>
2020-08-27 09:18:10 +0800

    a

--- pages/tableData/datapages/tptx/gylh/lcgc/lcgc.js
+++ pages/tableData/datapages/tptx/gylh/lcgc/lcgc.js
@@ -1,11 +1,40 @@
-// pages/tableData/datapages/tptx/gylh/lcgc/lcgc.js
+const app = getApp();
+const RootPath = "http://localhost:16000";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    pmGylhl:{},
+    lcgcTfgcAfterImgUrl: [], 
+    lcgcTfgcBeforeImgUrl: [], 
+    lcgcTfgcMidImgUrl: [], 
 
+    lcgcYlzxAfterImgUrl: [], 
+    lcgcYlzxBeforeImgUrl: [], 
+    lcgcYlzxMidImgUrl: [],
+
+    lcgcYlpzAfterImgUrl: [], 
+    lcgcYlpzBeforeImgUrl: [], 
+    lcgcYlpzMidImgUrl: [], 
+
+    lcgcDlzxAfterImgUrl: [], 
+    lcgcDlzxMidImgUrl: [], 
+    lcgcDlzxBeforeImgUrl: [], 
+
+    lcgcDlyhAfterImgUrl: [], 
+    lcgcDlyhBeforeImgUrl: [], 
+    lcgcDlyhMidImgUrl: [], 
+
+    lcgcTcczxAfterImgUrl: [], 
+    lcgcTcczxBeforeImgUrl: [], 
+    lcgcTcczxMidImgUrl: [], 
+
+    lcgcTccpzAfterImgUrl: [], 
+    lcgcTccpzBeforeImgUrl: [], 
+    lcgcTccpzMidImgUrl: [], 
+    RootPath: RootPath
   },
 
   /**
@@ -22,11 +51,76 @@ Page({
 
   },
 
+  //查询接口，onshow中调用
+  getYqgc: function(){
+    console.log("调用getYqgc");
+    var that = this;
+    wx.request({
+      url: RootPath + "/jk-gcgl/api/tptx/pmTptx/gylhl?pId="+14, //请求路径
+      method: 'post',
+      data: {
+        
+      },
+      header: {
+        'content-type': 'application/json', // 默认值
+        'thirdSession': app.globalData.thirdSession
+      },
+      success (res) {
+        if (res.statusCode == 200) {
+          console.log(res.data);
+          var qq = res.data.data
+          console.log(qq)
+          if (res.data.code == 200) {
+            that.setData({
+              pmGylhl: res.data.data,
+              lcgcTfgcAfterImgUrl : res.data.data.lcgcTfgcAfterImgUrl.split(","),
+              lcgcTfgcBeforeImgUrl : res.data.data.lcgcTfgcBeforeImgUrl.split(","),
+              lcgcTfgcMidImgUrl : res.data.data.lcgcTfgcMidImgUrl.split(","),
+
+              lcgcYlzxAfterImgUrl : res.data.data.lcgcYlzxAfterImgUrl.split(","),
+              lcgcYlzxBeforeImgUrl : res.data.data.lcgcYlzxBeforeImgUrl.split(","),
+              lcgcYlzxMidImgUrl : res.data.data.lcgcYlzxMidImgUrl.split(","),
+
+              lcgcYlpzAfterImgUrl : res.data.data.lcgcYlpzAfterImgUrl.split(","),
+              lcgcYlpzBeforeImgUrl :  res.data.data.lcgcYlpzBeforeImgUrl.split(","),
+              lcgcYlpzMidImgUrl : res.data.data.lcgcYlpzMidImgUrl.split(","),
+
+              lcgcDlzxAfterImgUrl : res.data.data.lcgcDlzxAfterImgUrl.split(","),
+              lcgcDlzxMidImgUrl : res.data.data.lcgcDlzxMidImgUrl.split(","),
+              lcgcDlzxBeforeImgUrl : res.data.data.lcgcDlzxBeforeImgUrl.split(","),
+
+              lcgcDlyhAfterImgUrl : res.data.data.lcgcDlyhAfterImgUrl.split(","),
+              lcgcDlyhBeforeImgUrl : res.data.data.lcgcDlyhBeforeImgUrl.split(","),
+              lcgcDlyhMidImgUrl : res.data.data.lcgcDlyhMidImgUrl.split(","),
+
+              lcgcTcczxAfterImgUrl : res.data.data.lcgcTcczxAfterImgUrl.split(","),
+              lcgcTcczxBeforeImgUrl : res.data.data.lcgcTcczxBeforeImgUrl.split(","),
+              lcgcTcczxMidImgUrl : res.data.data.lcgcTcczxMidImgUrl.split(","),
+
+              lcgcTccpzAfterImgUrl : res.data.data.lcgcTccpzAfterImgUrl.split(","),
+              lcgcTccpzBeforeImgUrl : res.data.data.lcgcTccpzBeforeImgUrl.split(","),
+              lcgcTccpzMidImgUrl : res.data.data.lcgcTccpzMidImgUrl.split(","),
+
+            })
+          }else{
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'success',
+              duration: 2000
+            });
+            console.log("finish.js getYqgc() error res:----->>>>");
+            console.log(res);
+          }
+        }
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this;
+    that.getYqgc();
   },
 
   /**
--- pages/tableData/datapages/tptx/gylh/whjg/whjg.js
+++ pages/tableData/datapages/tptx/gylh/whjg/whjg.js
@@ -1,11 +1,40 @@
-// pages/tableData/datapages/tptx/gylh/whjg/whjg.js
+const app = getApp();
+const RootPath = "http://localhost:16000";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    pmGylhl:{},
+    whjgSxzlAfterImgUrl: [], 
+    whjgSxzlBeforeImgUrl: [], 
+    whjgSxzlMidImgUrl: [], 
 
+    whjgMzdAfterImgUrl: [], 
+    whjgMzdBeforeImgUrl: [], 
+    whjgMzdMidImgUrl: [],
+
+    whjgJgqAfterImgUrl: [], 
+    whjgJgqBeforeImgUrl: [], 
+    whjgJgqMidImgUrl: [], 
+
+    whjgYsxpAfterImgUrl: [], 
+    whjgYsxpMidImgUrl: [], 
+    whjgYsxpBeforeImgUrl: [], 
+
+    whjgWhdsAfterImgUrl: [], 
+    whjgWhdsBeforeImgUrl: [], 
+    whjgWhdsMidImgUrl: [], 
+
+    whjgMhkhAfterImgUrl: [], 
+    whjgMhkhBeforeImgUrl: [], 
+    whjgMhkhMidImgUrl: [], 
+
+    whjgYtbzAfterImgUrl: [], 
+    whjgYtbzBeforeImgUrl: [], 
+    whjgYtbzMidImgUrl: [], 
+    RootPath: RootPath
   },
 
   /**
@@ -22,11 +51,76 @@ Page({
 
   },
 
+  //查询接口，onshow中调用
+  getYqgc: function(){
+    console.log("调用getYqgc");
+    var that = this;
+    wx.request({
+      url: RootPath + "/jk-gcgl/api/tptx/pmTptx/gylhl?pId="+14, //请求路径
+      method: 'post',
+      data: {
+        
+      },
+      header: {
+        'content-type': 'application/json', // 默认值
+        'thirdSession': app.globalData.thirdSession
+      },
+      success (res) {
+        if (res.statusCode == 200) {
+          console.log(res.data);
+          var qq = res.data.data
+          console.log(qq)
+          if (res.data.code == 200) {
+            that.setData({
+              pmGylhl: res.data.data,
+              whjgSxzlAfterImgUrl : res.data.data.whjgSxzlAfterImgUrl.split(","),
+              whjgSxzlBeforeImgUrl : res.data.data.whjgSxzlBeforeImgUrl.split(","),
+              whjgSxzlMidImgUrl : res.data.data.whjgSxzlMidImgUrl.split(","),
+
+              whjgMzdAfterImgUrl : res.data.data.whjgMzdAfterImgUrl.split(","),
+              whjgMzdBeforeImgUrl : res.data.data.whjgMzdBeforeImgUrl.split(","),
+              whjgMzdMidImgUrl : res.data.data.whjgMzdMidImgUrl.split(","),
+
+              whjgJgqAfterImgUrl : res.data.data.whjgJgqAfterImgUrl.split(","),
+              whjgJgqBeforeImgUrl :  res.data.data.whjgJgqBeforeImgUrl.split(","),
+              whjgJgqMidImgUrl : res.data.data.whjgJgqMidImgUrl.split(","),
+
+              whjgYsxpAfterImgUrl : res.data.data.whjgYsxpAfterImgUrl.split(","),
+              whjgYsxpMidImgUrl : res.data.data.whjgYsxpMidImgUrl.split(","),
+              whjgYsxpBeforeImgUrl : res.data.data.whjgYsxpBeforeImgUrl.split(","),
+
+              whjgWhdsAfterImgUrl : res.data.data.whjgWhdsAfterImgUrl.split(","),
+              whjgWhdsBeforeImgUrl : res.data.data.whjgWhdsBeforeImgUrl.split(","),
+              whjgWhdsMidImgUrl : res.data.data.whjgWhdsMidImgUrl.split(","),
+
+              whjgMhkhAfterImgUrl : res.data.data.whjgMhkhAfterImgUrl.split(","),
+              whjgMhkhBeforeImgUrl : res.data.data.whjgMhkhBeforeImgUrl.split(","),
+              whjgMhkhMidImgUrl : res.data.data.whjgMhkhMidImgUrl.split(","),
+
+              whjgYtbzAfterImgUrl : res.data.data.whjgYtbzAfterImgUrl.split(","),
+              whjgYtbzBeforeImgUrl : res.data.data.whjgYtbzBeforeImgUrl.split(","),
+              whjgYtbzMidImgUrl : res.data.data.whjgYtbzMidImgUrl.split(","),
+		
+            })
+          }else{
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'success',
+              duration: 2000
+            });
+            console.log("finish.js getYqgc() error res:----->>>>");
+            console.log(res);
+          }
+        }
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this;
+    that.getYqgc();
   },
 
   /**
