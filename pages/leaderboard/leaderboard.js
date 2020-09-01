5aea56da129a85a4e0df2926ef5c6a710a61879c
wuvsxi <974142709@qq.com>
2020-09-01 10:39:36 +0800

    排行榜页面修改

--- pages/leaderboard/leaderboard.js
+++ pages/leaderboard/leaderboard.js
@@ -1,11 +1,22 @@
 // pages/charts/charts.js
 const app = getApp();
+var basePath = app.globalData.basePath;
 Page({
 
 // -------------------- 数据区域 --------------------
   data: {
     rankData: [
       
+    ],
+    titleList: [
+      // {columnName: 'integrated', title: '综合排名'},
+      {columnName: 'projectNum', title: '项目个数',data: []},
+      {columnName: 'standardsNum', title: '达标项目',data: []},
+      {columnName: 'standardsRate', title: '达标率',data: []},
+      {columnName: 'buildArea', title: '建筑面积',data: []},
+      {columnName: 'totalInvestment', title: '总投资',data: []},
+      {columnName: 'projectInvestment', title: '工程投资',data: []},
+      {columnName: 'monthOutput', title: '本月产值',data: []},
     ]
   },
 
@@ -21,7 +32,7 @@ onLoad: function (options) {
 getRankData: function(){
   var that = this;
   wx.request({
-    url: "http://localhost:16000/jk-gcgl/api/leaderboard/leaderboard/list", //请求路径
+    url: basePath + "/api/leaderboard/leaderboard/list", //请求路径
     method: 'post',
     data: {
 
@@ -34,8 +45,49 @@ getRankData: function(){
       if (res.statusCode == 200) {
         console.log(res.data);
         if (res.data.code == 200) {
+          let dataList = res.data.data
+          let titleList = that.data.titleList
+          for(let i=0;i<dataList.length;i++) {
+            if (dataList[i].projectNum) {
+              titleList[0].data.push(dataList[i].projectNum)
+            } else {
+              titleList[0].data.push('-')
+            }
+            if (dataList[i].standardsNum) {
+              titleList[1].data.push(dataList[i].standardsNum)
+            } else {
+              titleList[1].data.push('-')
+            }
+            if (dataList[i].standardsRate) {
+              titleList[2].data.push(dataList[i].standardsRate)
+            } else {
+              titleList[2].data.push('-')
+            }
+            if (dataList[i].buildArea) {
+              titleList[3].data.push(dataList[i].buildArea)
+            } else {
+              titleList[3].data.push('-')
+            }
+            if (dataList[i].totalInvestment) {
+              titleList[4].data.push(dataList[i].totalInvestment)
+            } else {
+              titleList[4].data.push('-')
+            }
+            if (dataList[i].projectInvestment) {
+              titleList[5].data.push(dataList[i].projectInvestment)
+            } else {
+              titleList[5].data.push('-')
+            }
+            if (dataList[i].monthOutput) {
+              titleList[6].data.push(dataList[i].monthOutput)
+            } else {
+              titleList[6].data.push('-')
+            }
+          }
+          console.log(titleList)
           that.setData({
             rankData: res.data.data,
+            titleList: titleList
           })
         }else{
           wx.showToast({
