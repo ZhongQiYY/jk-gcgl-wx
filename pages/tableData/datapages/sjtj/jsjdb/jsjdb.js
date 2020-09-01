aa897c659339004fdebaba2e145eb692af0207a8
ZhongQiYY <2378544112@qq.com>
2020-09-01 22:51:59 +0800

    commit

--- pages/tableData/datapages/sjtj/jsjdb/jsjdb.js
+++ pages/tableData/datapages/sjtj/jsjdb/jsjdb.js
@@ -1,11 +1,22 @@
 // pages/tableData/datapages/sjtj/jsjdb/jsjdb.js
+var app = getApp();
+var basePath = app.globalData.basePath;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    timeInfo: {},
+    listData:[
+      {"code":"01","text":"text1","type":"type1"},
+      {"code":"02","text":"text2","type":"type2"},
+      {"code":"03","text":"text3","type":"type3"},
+      {"code":"04","text":"text4","type":"type4"},
+      {"code":"05","text":"text5","type":"type5"},
+      {"code":"06","text":"text6","type":"type6"},
+      {"code":"07","text":"text7","type":"type7"}
+    ]
   },
 
   /**
@@ -26,7 +37,25 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this;
+    wx.request({
+      url: basePath+"/api/project/projectScheduleInfo", //请求路径
+      method: 'post',
+      data: {
+        projectId: app.globalData.projectId,
+        categoryType: app.globalData.categoryType
+      },
+      header: {
+        'content-type': 'application/json', // 默认值
+        'thirdSession': app.globalData.thirdSession
+      },
+      success (res) {
+        that.setData({
+          timeInfo: res.data
+        });
+        console.log(that.data.timeInfo);
+      }
+    });
   },
 
   /**
