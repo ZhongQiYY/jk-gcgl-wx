6b293afc3d7fb66e70b0f232dbae4bfb542dfe7f
ZhongQiYY <2378544112@qq.com>
2020-09-17 16:13:36 +0800

    commit

--- pages/project/project.js
+++ pages/project/project.js
@@ -5,24 +5,26 @@ Page({
   mixins: [require('../../dist/mixin/themeChanged')],
 
   data: {
-    buildArray: ['--请选择--','科技城', '满园', '西城', '综保区', '中恒工业', '中恒商业', '磊昇', '工建'],
-    categoryArray: ['--请选择--','返迁棚改', '工业厂房', '商业地产', '文教体卫', '公园绿化', '市政桥梁'],
+    buildArray: ['--请选择--', '科技城', '满园', '西城', '综保区', '中恒工业', '中恒商业', '磊昇', '工建'],
+    categoryArray: ['--请选择--', '返迁棚改', '工业厂房', '商业地产', '文教体卫', '公园绿化', '市政桥梁'],
     rootPath: "https://test.xizinet.com",
     buildIndex: 0,
     categoryIndex: 0,
     projectList: [],
     search: "search",
     loadingHidden: false,
+    hasUserInfo: false,
+    errorInfo: true
   },
 
   // 选择建设单位
-  bindBuildChange: function(e) {  
+  bindBuildChange: function (e) {
     var that = this;
     this.setData({
       buildIndex: e.detail.value
     });
     wx.request({
-      url: basePath+"/api/project/list", //请求路径
+      url: basePath + "/api/project/list", //请求路径
       method: 'post',
       data: {
         unitName: this.data.buildArray[e.detail.value],
@@ -32,7 +34,7 @@ Page({
         'content-type': 'application/json', // 默认值
         'thirdSession': app.globalData.thirdSession
       },
-      success (res) {
+      success(res) {
         that.setData({
           projectList: res.data
         })
@@ -40,23 +42,23 @@ Page({
     });
   },
   // 选择项目类别
-  bindCategoryChange: function(e) {
+  bindCategoryChange: function (e) {
     var that = this;
     this.setData({
       categoryIndex: e.detail.value
     });
     wx.request({
-      url: basePath+"/api/project/list", //请求路径
+      url: basePath + "/api/project/list", //请求路径
       method: 'post',
       data: {
         unitName: this.data.buildArray[this.data.buildIndex],
-        categoryId: this.data.categoryIndex 
+        categoryId: this.data.categoryIndex
       },
       header: {
         'content-type': 'application/json', // 默认值
         'thirdSession': app.globalData.thirdSession
       },
-      success (res) {
+      success(res) {
         that.setData({
           projectList: res.data
         })
@@ -65,96 +67,104 @@ Page({
   },
 
   //显示项目详情
-  showProjectInfo: function(e) {
+  showProjectInfo: function (e) {
     app.globalData.projectId = e.currentTarget.dataset.id;
     app.globalData.categoryType = e.currentTarget.dataset.categorytype;
     app.globalData.pName = e.currentTarget.dataset.text
-    var that = this;
-    this.setData({
-      // search: "search1"
-    });
-
     wx.navigateTo({
       url: '/pages/project/projectInfo/projectInfo',
     })
 
-    
+
   },
 
-  
-
-  //显示项目列表
-  showProjectList: function(e) {
+  //下拉触发
+  onPullDownRefresh: function () {
     var that = this;
-    this.setData({
-      // search: "search"
-    });
-    wx.request({
-      url: basePath+"/api/project/list", //请求路径
-      method: 'post',
-      data: {
-        unitName: this.data.buildArray[this.data.buildIndex],
-        categoryId: this.data.categoryIndex 
-      },
-      header: {
-        'content-type': 'application/json', // 默认值
-        'thirdSession': app.globalData.thirdSession
-      },
-      success (res) {
-        that.setData({
-          projectList: res.data
-        })
-      }
-    });
+    that.onLoad();
   },
 
-  
-  
+  //--------------------------生命周期函数------------------------------
+  onLoad: function (options) {
+    var that = this;
+    wx.stopPullDownRefresh();
+    that.setData({
+      hasUserInfo: app.globalData.hasUserInfo
+    });
 
-//--------------------------生命周期函数------------------------------
-
-
-
-  onLoad: function(options){
-      let page = this
-      setTimeout(function(){
-        page.setData({
-            loadingHidden: true
-        });
-      }, 900);
-      var that = this;
-    var interval = setInterval(function(){
-      wx.request({
-        url: basePath+"/api/project/list", //请求路径
-        method: 'post',
-        data: {
-          unitName: that.data.buildArray[that.data.buildIndex],
-          categoryId: that.data.categoryIndex 
-        },
-        header: {
-          'content-type': 'application/json', // 默认值
-          'thirdSession': app.globalData.thirdSession
-        },
-        success (res) {
-          that.setData({
-            projectList: res.data
-          });
-          clearInterval(interval);
-        }
-      });
-    },1000);
-      this.setData({
-          icon: base64.icon20
-      });
-      app.userLogin(function(){
-        page.setData({
+    app.userLogin(
+      function () {
+        that.setData({
           hasUserInfo: app.globalData.hasUserInfo
         });
+        if (that.data.hasUserInfo) {
+          that.setData({
+            loadingHidden: false,
+            errorInfo: true
+          })
+          wx.request({
+            url: basePath + "/api/project/list", //请求路径
+            method: 'post',
+            data: {
+              unitName: that.data.buildArray[that.data.buildIndex],
+              categoryId: that.data.categoryIndex
+            },
+            header: {
+              'content-type': 'application/json', // 默认值
+              'thirdSession': app.globalData.thirdSession
+            },
+            success(res) {
+              that.setData({
+                projectList: res.data,
+                loadingHidden: true,
+                errorInfo: true
+              });
+            },
+            fail() {
+              that.setData({
+                loadingHidden: false,
+                errorInfo: true
+              })
+              setTimeout(function () {
+                that.setData({
+                  loadingHidden: true,
+                  errorInfo: false
+                })
+              }, 10000);
+            }
+          });
+        } else {
+          that.setData({
+            loadingHidden: false,
+            errorInfo: true
+          })
+          setTimeout(function () {
+            that.setData({
+              loadingHidden: true,
+              errorInfo: false
+            })
+          }, 10000);
+        }
+      },
+      function () {
+        that.setData({
+          loadingHidden: false,
+          errorInfo: true
+        })
+        setTimeout(function () {
+          that.setData({
+            loadingHidden: true,
+            errorInfo: false
+          })
+        }, 10000);
       });
 
+    this.setData({
+      icon: base64.icon20
+    });
   },
 
-  onShow: function(){
-    
+  onShow: function () {
+
   }
 })
--- pages/project/projectInfo/projectInfo.js
+++ pages/project/projectInfo/projectInfo.js
@@ -18,47 +18,44 @@ Page({
     coverArea: "亩",
     projectName: "",
     rootPath: "https://test.xizinet.com",
-    categoryType: 0
+    categoryType: 0,
+    loadingHidden: false,
   },
 
-
-
   //跳转到数据表界面
-  toTableData: function() {
+  toTableData: function () {
     app.globalData.pName = this.data.projectName;
-    wx.switchTab({ 
+    wx.switchTab({
       url: '/pages/tableData/tableData'
     });
   },
 
-  
-
   //点击五方信息触发
-  towfxx: function(e) {
-    setTimeout(function(){
+  towfxx: function (e) {
+    setTimeout(function () {
       wx.pageScrollTo({
         duration: 300,
         selector: '#wfxx'
         // scrollTop: 0
       });
-    },600);
+    }, 600);
     this.setData({
       isWfxxShow: !this.data.isWfxxShow,
       isJbxxShow: false,
       isZjqkShow: false,
       isDtqkShow: false
     })
-    
+
   },
   //点击基本信息触发
-  toJbxx: function(e) {
-    setTimeout(function(){
+  toJbxx: function (e) {
+    setTimeout(function () {
       wx.pageScrollTo({
         duration: 300,
         selector: '.jbxx'
         // scrollTop: 0
       });
-    },600);
+    }, 600);
     this.setData({
       isJbxxShow: !this.data.isJbxxShow,
       isWfxxShow: false,
@@ -67,15 +64,15 @@ Page({
     })
   },
   //点击资金信息触发
-  toZjqk: function(e) {
-    setTimeout(function(){
+  toZjqk: function (e) {
+    setTimeout(function () {
       wx.pageScrollTo({
         duration: 300,
         selector: '.zjqk'
         // scrollTop: 0
       });
-    },600);
-    
+    }, 600);
+
     this.setData({
       isZjqkShow: !this.data.isZjqkShow,
       isWfxxShow: false,
@@ -84,15 +81,15 @@ Page({
     })
   },
   //点击单体情况触发
-  toDtqk: function(e) {
-    setTimeout(function(){
+  toDtqk: function (e) {
+    setTimeout(function () {
       wx.pageScrollTo({
         duration: 300,
         selector: '.dtqk'
         // scrollTop: 0
       });
-    },600);
-    
+    }, 600);
+
     this.setData({
       isDtqkShow: !this.data.isDtqkShow,
       isWfxxShow: false,
@@ -101,14 +98,47 @@ Page({
     })
   },
 
-
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function () {
+    var that = this;
+    wx.stopPullDownRefresh();
+    that.setData({
+      projectName: app.globalData.pName,
+      categoryType: app.globalData.categoryType,
+    })
+    wx.request({
+      url: basePath + "/api/project/projectBaseInfo", //请求路径
+      method: 'post',
+      data: {
+        projectId: app.globalData.projectId,
+        categoryType: app.globalData.categoryType
+      },
+      header: {
+        'content-type': 'application/json', // 默认值
+        'thirdSession': app.globalData.thirdSession
+      },
+      success(res) {
+        that.setData({ 
+          projectInfo: res.data,  
+          loadingHidden: true,
+          imagePath: that.data.rootPath + res.data.effectPicture   
+        });
+      }
+    });
   },
+
+  //下拉触发
+  onPullDownRefresh: function() {
+    var that = this;
+    that.onLoad();
+  },
+
+
+
+
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -121,30 +151,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    var that = this;
-    wx.request({
-      url: basePath+"/api/project/projectBaseInfo", //请求路径
-      method: 'post',
-      data: {
-        projectId: app.globalData.projectId,
-        categoryType: app.globalData.categoryType
-      },
-      header: {
-        'content-type': 'application/json', // 默认值
-        'thirdSession': app.globalData.thirdSession
-      },
-      success (res) {
-        that.setData({
-          projectName: app.globalData.pName,
-          projectInfo: res.data,
-          categoryType: app.globalData.categoryType
-        });
-        console.log();
-        that.setData({
-          imagePath: that.data.rootPath+res.data.effectPicture
-        })
-      }
-    });
+    
   },
 
   /**
