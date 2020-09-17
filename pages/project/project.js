128afd5430ec936e2720b4687e454597fd8ab6fb
ZhongQiYY <2378544112@qq.com>
2020-09-17 10:07:08 +0800

    commit

--- pages/project/project.js
+++ pages/project/project.js
@@ -11,21 +11,8 @@ Page({
     buildIndex: 0,
     categoryIndex: 0,
     projectList: [],
-    projectListShow: true,
-    projectInfoShow: false,
-    projectName: "",
     search: "search",
-    imagePath: "",
-    categoryType: 0,
-    projectInfo: {},
-    isJbxxShow: false,
-    isWfxxShow: false,
-    isZjqkShow: false,
-    isDtqkShow: false,
     loadingHidden: false,
-    moneyUnit: "万元",
-    buildArea: "万平米",
-    coverArea: "亩",
   },
 
   // 选择建设单位
@@ -81,53 +68,26 @@ Page({
   showProjectInfo: function(e) {
     app.globalData.projectId = e.currentTarget.dataset.id;
     app.globalData.categoryType = e.currentTarget.dataset.categorytype;
+    app.globalData.pName = e.currentTarget.dataset.text
     var that = this;
     this.setData({
-      projectListShow: false,
-      projectInfoShow: true,
-      projectName: e.currentTarget.dataset.text,
-      categoryType: e.currentTarget.dataset.categorytype,
-      search: "search1"
+      // search: "search1"
     });
 
-    wx.request({
-      url: basePath+"/api/project/projectBaseInfo", //请求路径
-      method: 'post',
-      data: {
-        projectId: app.globalData.projectId,
-        categoryType: app.globalData.categoryType
-      },
-      header: {
-        'content-type': 'application/json', // 默认值
-        'thirdSession': app.globalData.thirdSession
-      },
-      success (res) {
-        console.log(res.data)
-        that.setData({
-          projectInfo: res.data
-        });
-        that.setData({
-          imagePath: that.data.rootPath+res.data.effectPicture
-        })
-      }
-    });
+    wx.navigateTo({
+      url: '/pages/project/projectInfo/projectInfo',
+    })
+
+    
   },
 
-  //跳转到数据表界面
-  toTableData: function() {
-    app.globalData.pName = this.data.projectName;
-    wx.switchTab({ 
-      url: '/pages/tableData/tableData'
-    });
-  },
+  
 
   //显示项目列表
   showProjectList: function(e) {
     var that = this;
     this.setData({
-      projectListShow: true,
-      projectInfoShow: false,
-      search: "search"
+      // search: "search"
     });
     wx.request({
       url: basePath+"/api/project/list", //请求路径
@@ -148,73 +108,7 @@ Page({
     });
   },
 
-  //点击五方信息触发
-  towfxx: function(e) {
-    setTimeout(function(){
-      wx.pageScrollTo({
-        duration: 300,
-        selector: '#wfxx'
-        // scrollTop: 0
-      });
-    },600);
-    this.setData({
-      isWfxxShow: !this.data.isWfxxShow,
-      isJbxxShow: false,
-      isZjqkShow: false,
-      isDtqkShow: false
-    })
-    
-  },
-  //点击基本信息触发
-  toJbxx: function(e) {
-    setTimeout(function(){
-      wx.pageScrollTo({
-        duration: 300,
-        selector: '.jbxx'
-        // scrollTop: 0
-      });
-    },600);
-    this.setData({
-      isJbxxShow: !this.data.isJbxxShow,
-      isWfxxShow: false,
-      isZjqkShow: false,
-      isDtqkShow: false
-    })
-  },
-  //点击资金信息触发
-  toZjqk: function(e) {
-    setTimeout(function(){
-      wx.pageScrollTo({
-        duration: 300,
-        selector: '.zjqk'
-        // scrollTop: 0
-      });
-    },600);
-    
-    this.setData({
-      isZjqkShow: !this.data.isZjqkShow,
-      isWfxxShow: false,
-      isJbxxShow: false,
-      isDtqkShow: false
-    })
-  },
-  //点击单体情况触发
-  toDtqk: function(e) {
-    setTimeout(function(){
-      wx.pageScrollTo({
-        duration: 300,
-        selector: '.dtqk'
-        // scrollTop: 0
-      });
-    },600);
-    
-    this.setData({
-      isDtqkShow: !this.data.isDtqkShow,
-      isWfxxShow: false,
-      isJbxxShow: false,
-      isZjqkShow: false,
-    })
-  },
+  
   
 
 //--------------------------生命周期函数------------------------------
@@ -228,19 +122,7 @@ Page({
             loadingHidden: true
         });
       }, 900);
-      this.setData({
-          icon: base64.icon20
-      });
-      app.userLogin(function(){
-        page.setData({
-          hasUserInfo: app.globalData.hasUserInfo
-        });
-      });
-
-  },
-
-  onShow: function(){
-    var that = this;
+      var that = this;
     var interval = setInterval(function(){
       wx.request({
         url: basePath+"/api/project/list", //请求路径
@@ -261,5 +143,18 @@ Page({
         }
       });
     },1000);
+      this.setData({
+          icon: base64.icon20
+      });
+      app.userLogin(function(){
+        page.setData({
+          hasUserInfo: app.globalData.hasUserInfo
+        });
+      });
+
+  },
+
+  onShow: function(){
+    
   }
 })
--- pages/project/projectInfo/projectInfo.js
+++ pages/project/projectInfo/projectInfo.js
@@ -1,16 +1,105 @@
 // pages/project/projectInfo/projectInfo.js
+var app = getApp();
+var basePath = app.globalData.basePath;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    id: 0
+    imagePath: "",
+    projectInfo: {},
+    isJbxxShow: false,
+    isWfxxShow: false,
+    isZjqkShow: false,
+    isDtqkShow: false,
+    moneyUnit: "万元",
+    buildArea: "万平米",
+    coverArea: "亩",
+    projectName: "",
+    rootPath: "https://test.xizinet.com",
+    categoryType: 0
   },
 
 
+
+  //跳转到数据表界面
+  toTableData: function() {
+    app.globalData.pName = this.data.projectName;
+    wx.switchTab({ 
+      url: '/pages/tableData/tableData'
+    });
+  },
+
   
 
+  //点击五方信息触发
+  towfxx: function(e) {
+    setTimeout(function(){
+      wx.pageScrollTo({
+        duration: 300,
+        selector: '#wfxx'
+        // scrollTop: 0
+      });
+    },600);
+    this.setData({
+      isWfxxShow: !this.data.isWfxxShow,
+      isJbxxShow: false,
+      isZjqkShow: false,
+      isDtqkShow: false
+    })
+    
+  },
+  //点击基本信息触发
+  toJbxx: function(e) {
+    setTimeout(function(){
+      wx.pageScrollTo({
+        duration: 300,
+        selector: '.jbxx'
+        // scrollTop: 0
+      });
+    },600);
+    this.setData({
+      isJbxxShow: !this.data.isJbxxShow,
+      isWfxxShow: false,
+      isZjqkShow: false,
+      isDtqkShow: false
+    })
+  },
+  //点击资金信息触发
+  toZjqk: function(e) {
+    setTimeout(function(){
+      wx.pageScrollTo({
+        duration: 300,
+        selector: '.zjqk'
+        // scrollTop: 0
+      });
+    },600);
+    
+    this.setData({
+      isZjqkShow: !this.data.isZjqkShow,
+      isWfxxShow: false,
+      isJbxxShow: false,
+      isDtqkShow: false
+    })
+  },
+  //点击单体情况触发
+  toDtqk: function(e) {
+    setTimeout(function(){
+      wx.pageScrollTo({
+        duration: 300,
+        selector: '.dtqk'
+        // scrollTop: 0
+      });
+    },600);
+    
+    this.setData({
+      isDtqkShow: !this.data.isDtqkShow,
+      isWfxxShow: false,
+      isJbxxShow: false,
+      isZjqkShow: false,
+    })
+  },
 
 
 
@@ -18,10 +107,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      id: options.id
-    });
-    //用拿到的id去查项目详情
+    
   },
 
   /**
@@ -35,7 +121,30 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this;
+    wx.request({
+      url: basePath+"/api/project/projectBaseInfo", //请求路径
+      method: 'post',
+      data: {
+        projectId: app.globalData.projectId,
+        categoryType: app.globalData.categoryType
+      },
+      header: {
+        'content-type': 'application/json', // 默认值
+        'thirdSession': app.globalData.thirdSession
+      },
+      success (res) {
+        that.setData({
+          projectName: app.globalData.pName,
+          projectInfo: res.data,
+          categoryType: app.globalData.categoryType
+        });
+        console.log();
+        that.setData({
+          imagePath: that.data.rootPath+res.data.effectPicture
+        })
+      }
+    });
   },
 
   /**
