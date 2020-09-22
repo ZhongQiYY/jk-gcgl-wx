f0fd19f4544e09d1f91dd11ce151c90551cbe1ed
18779918803 <814883929@qq.com>
2020-09-22 08:32:08 +0800

    a

--- pages/control/tjtptx/gylh/whjg/whjg.js
+++ pages/control/tjtptx/gylh/whjg/whjg.js
@@ -1,4 +1,4 @@
-// pages/control/tjtptx/gylh/whjg/whjg.js
+// pages/tableData/datapages/tptx/tptx.js
 Page({
 
   /**
@@ -62,5 +62,69 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  /**
+   * 水系整理
+   */
+  toDetailsAndDxzx: function () {
+    wx.navigateTo({
+      url: '/pages/control/tjtptx/gylh/whjg/sxzl/sxzl'
+    })
+
+  },
+
+  /**
+   * 木栈道
+   */
+  toDetailsAndJps: function () {
+    wx.navigateTo({
+      url: '/pages/control/tjtptx/gylh/whjg/mzd/mzd'
+    })
+  },
+
+   /**
+   * 景观桥
+   */
+  toDetailsAndLhzz: function () {
+    wx.navigateTo({
+      url: '/pages/control/tjtptx/gylh/whjg/jgq/jgq'
+    })
+  },
+
+  /**
+   * 艺术小品
+   */
+  toDetailsAndJgxp: function () {
+    wx.navigateTo({
+      url: '/pages/control/tjtptx/gylh/whjg/ysxp/ysxp'
+    })
+  },
+
+  /**
+   * 文化雕塑
+   */
+  toDetailsAndLdzm: function () {
+    wx.navigateTo({
+      url: '/pages/control/tjtptx/gylh/whjg/whds/whds'
+    })
+  },
+
+  /**
+   * 美化刻画
+   */
+  toDetailsAndJkzn: function () {
+    wx.navigateTo({
+      url: '/pages/control/tjtptx/gylh/whjg/mhkh/mhkh'
+    })
+  },
+
+   /**
+   * 业态布展
+   */
+  toDetailsAndLxgc: function () {
+    wx.navigateTo({
+      url: '/pages/control/tjtptx/gylh/whjg/ytbz/ytbz'
+    })
+  },
 })
