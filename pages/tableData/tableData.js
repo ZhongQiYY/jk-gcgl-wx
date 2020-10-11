3fe8ec9213ac8c55f5fa0e3cc709b950c6594dd0
ZhongQiYY <2378544112@qq.com>
2020-10-12 04:15:43 +0800

    commit

--- pages/tableData/tableData.js
+++ pages/tableData/tableData.js
@@ -1,5 +1,6 @@
 // pages/tableData/tableData.js
 var app = getApp();
+var basePath = app.globalData.basePath;
 Page({
 
   mixins: [require('../../dist/mixin/themeChanged')],
@@ -8,6 +9,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    projectNames: [], //@@
     projectName: ""
   },
 
@@ -68,15 +70,47 @@ Page({
   },
 
   toYdxx: function() {
-    if(app.globalData.projectId != 0 && app.globalData.categoryType == 1) {
+    if(app.globalData.projectId != 0 && app.globalData.categoryType != 0) {
       wx.navigateTo({
         url: '/pages/tableData/datapages/ydxx/ydxx',
       })
     }
   },
 
+  toTxsx: function(){
+    if(app.globalData.projectId != 0 && app.globalData.categoryType != 0) {
+      wx.navigateTo({
+        url: '/pages/tableData/datapages/txsx/txsx',
+      })
+    }
+  },
 
 
+//搜索框组件返回的方法 @@
+inputTyping: function (e) {
+  var inputVal = e.detail.inputVal;
+  var projectNames1 = [];
+  if (inputVal.length > 0) {
+    for (const nl of app.globalData.projectNameList) {
+      var projectName = nl.projectName;
+      if (projectName.indexOf(inputVal) != -1) {
+        projectNames1.push(nl);
+      }
+    }
+  }
+  this.setData({
+    projectNames: projectNames1
+  })
+},
+//搜索框组件返回的方法 
+selectProject: function (e) {
+  app.globalData.pName = e.detail.projectName;
+  app.globalData.projectId = e.detail.projectId;
+  app.globalData.categoryType = e.detail.categoryType;
+  this.setData({
+    projectName: e.detail.projectName
+  });
+},
 
 
 
@@ -92,7 +126,24 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var list = app.globalData.projectNameList;
+    if(list.length <= 0){
+      console.log("zhongqi")
+      wx.request({
+        url: basePath+"/api/project/nameList", //请求路径
+        method: 'post',
+        data: {
+          
+        },
+        header: {
+          'content-type': 'application/json', // 默认值
+          'thirdSession': app.globalData.thirdSession
+        },
+        success (res) {
+          app.globalData.projectNameList = res.data  
+        }
+      });
+    }
   },
 
   /**
