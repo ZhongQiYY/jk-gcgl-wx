5994c7129db960e8c934cbca37f8f631be59b4c2
18779918803 <814883929@qq.com>
2020-10-19 15:38:43 +0800

    a

--- pages/tableData/datapages/tptx/fjl/ztgc/ztgc.js
+++ pages/tableData/datapages/tptx/fjl/ztgc/ztgc.js
@@ -1,11 +1,16 @@
-// pages/tableData/datapages/tptx/fjl/ztgc/ztgc.js
+const app = getApp();
+//  const RootPath = "http://localhost:16000";
+const RootPath = app.globalData.imageRootPath;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    ztName: [],
+	  // ztAfterUrl: [], 
+    // ztBeforeUrl: [], 
+    // ztMidUrl: [], 
   },
 
   /**
@@ -22,11 +27,52 @@ Page({
 
   },
 
+  //查询接口，onshow中调用
+  getYqgc: function(){
+    console.log("调用getYqgc");
+    var that = this;
+    wx.request({
+      // url: RootPath + "/jk-gcgl/api/fjl/pmTptx/ztgc?pId="+app.globalData.projectId, //请求路径
+      url: RootPath + "/jk-gcgl/api/fjl/pmTptx/ztgc?pId="+77, //请求路径
+      method: 'post',
+      data: {
+        
+      },
+      header: {
+        'content-type': 'application/json', // 默认值
+        'thirdSession': app.globalData.thirdSession
+      },
+      success (res) {
+        if (res.statusCode == 200) {
+          console.log(res.data);
+          var qq = res.data.data
+          console.log(qq)
+          if (res.data.code == 200) {
+            that.setData({
+              ztName: res.data.data,
+              // ztAfterUrl : res.data.data.ztAfterUrl.split(","),
+              // ztBeforeUrl : res.data.data.ztBeforeUrl.split(","),
+              // ztMidUrl : res.data.data.ztMidUrl.split(","),
+            })
+          }else{
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'success',
+              duration: 2000
+            });
+            console.log("finish.js getYqgc() error res:----->>>>");
+            console.log(res);
+          }
+        }
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this;
+    that.getYqgc();
   },
 
   /**
