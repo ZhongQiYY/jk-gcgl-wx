c9afdf278aa66ada36d6c26445ad667ebb5c2823
ZhongQiYY <2378544112@qq.com>
2020-11-10 15:59:20 +0800

    commit

--- pages/control/txxmxx/txxmxx.js
+++ pages/control/txxmxx/txxmxx.js
@@ -1,12 +1,16 @@
+var app = getApp();
+var request = app.globalData.request;
+var requestUrl = app.globalData.requestUrl;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    projectNameListByCategory: [],
     active: 0,//tabbar索引
     showPlanSchedule: true,//展示进度计划
-
+    showProjectInfo: false,//展示项目信息
   },
   // 跳转到填写计划
   totxPlan: function(){
@@ -15,7 +19,13 @@ Page({
     })
   },
 
-
+  onLoad: function(e){
+    var that = this;
+    that.setData({
+      projectNameListByCategory: app.globalData.projectNameListByCategory,
+      showLoading: false
+    });
+  },
 
 
   tabbarChange: function(e){
@@ -26,21 +36,25 @@ Page({
     switch(e.detail){
       case 0:
         that.setData({
-          showPlanSchedule: true
+          showPlanSchedule: true,
+          showProjectInfo: false
         });
         break;
       case 1:
         that.setData({
+          showProjectInfo: true,
           showPlanSchedule: false
         });
         break;
       case 2:
         that.setData({
+          showProjectInfo: false,
           showPlanSchedule: false
         });
         break;
       case 3:
         that.setData({
+          showProjectInfo: false,
           showPlanSchedule: false
         });
         break;
