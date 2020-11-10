d8c55a03c040eecc6e89c9ca48455f4c7655c447
18779918803 <814883929@qq.com>
2020-11-10 10:27:24 +0800

    a

--- pages/control/tjtptx/tjtptx.js
+++ pages/control/tjtptx/tjtptx.js
@@ -6,9 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    uploaderList: [],
-    uploaderNum:0,
-    showUpload:true,
+    fileList: [],
     projectNames: [], //@@
     projectId: "", //@@
     categoryType: "", //@@
@@ -38,58 +36,23 @@ Page({
       projectName: e.detail.projectName
     });
   },
-
-   // 删除图片
-   clearImg:function(e){
-    var nowList = [];//新数据
-    var uploaderList = this.data.uploaderList;//原数据
-    
-    for (let i = 0; i < uploaderList.length;i++){
-        if (i == e.currentTarget.dataset.index){
-            continue;
-        }else{
-            nowList.push(uploaderList[i])
-        }
-    }
-    this.setData({
-        uploaderNum: this.data.uploaderNum - 1,
-        uploaderList: nowList,
-        showUpload: true
-    })
-},
-//展示图片
-showImg:function(e){
-    var that=this;
-    wx.previewImage({
-        urls: that.data.uploaderList,
-        current: that.data.uploaderList[e.currentTarget.dataset.index]
-    })
-},
-//上传图片
-upload: function(e) {
-    var that = this;
-    wx.chooseImage({
-        count: 9 - that.data.uploaderNum, // 默认9
-        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-        success: function(res) {
-            console.log(res)
-            // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-            let tempFilePaths = res.tempFilePaths;
-            let uploaderList = that.data.uploaderList.concat(tempFilePaths);
-            if (uploaderList.length==9){
-                that.setData({
-                    showUpload:false
-                })
-            }
-            that.setData({
-                uploaderList: uploaderList,
-                uploaderNum: uploaderList.length,
-            })
-        }
-    })
-},
-
+  afterRead(event) {
+    const { file } = event.detail;
+    // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+    wx.uploadFile({
+      url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
+      filePath: file.path,
+      name: 'file',
+      formData: { user: 'test' },
+      success(res) {
+        // 上传完成需要更新 fileList
+        const { fileList = [] } = this.data;
+        fileList.push({ ...file, url: res.data });
+        this.setData({ fileList });
+      },
+    });
+  },
+   
   /**
    * 生命周期函数--监听页面加载
    */
@@ -144,31 +107,5 @@ upload: function(e) {
    */
   onShareAppMessage: function () {
 
-  },
-
-  /**
-   * 房建类
-   */
-  toDetailsAndFjl: function (){
-    wx.navigateTo({
-      url: '/pages/control/tjtptx/fjl/fjl'
-    })
-  },
-  /**
-   * 公园绿化类
-   */
-  toDetailsAndGylhl: function () {
-    wx.navigateTo({
-      url: '/pages/control/tjtptx/gylh/gylh'
-    })
-  },
-
-  /**
-   * 市政桥梁类
-   */
-  toDetailsAndSzqll: function () {
-    wx.navigateTo({
-      url: '/pages/control/tjtptx/szql/szql'
-    })
   },
 })
